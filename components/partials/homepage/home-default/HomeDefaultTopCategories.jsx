e12068a925c1242feef532c702839302fd5fa11d
manoj.infoways <manoj.infoways@gmail.com>
2020-07-05 21:55:14 +0530

    upload first

---
+++ components/partials/homepage/home-default/HomeDefaultTopCategories.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import Link from 'next/link';
+
+const HomeDefaultTopCategories = () => (
+    <div className="ps-top-categories">
+        <div className="ps-container">
+            <h3>Top categories of the month</h3>
+            <div className="row">
+                {/* <div className="panel-item" data-url="/rakhi" data-selectioncode="ra-day">
+                <a href="/rakhi">
+                <div className="panel-image">
+                     <img src="https://cdn.igp.com/f_auto,q_auto/banners/rakhi-igp-20200529.jpg" data-original="https://cdn.igp.com/f_auto,q_auto/banners/rakhi-igp-20200529.jpg" alt="Father's day Gifts" className="img-responsive lazy">
+                </div>
+                <div className="panel-info">
+                <p class="panel-info-text">Rakhi</p>
+                </div>
+                </a>
+                </div> */}
+                 
+                <div className="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div className="ps-block--category">
+                        <Link href="/shop">
+                            <a className="ps-block__overlay"></a>
+                        </Link>
+                        <img src="/static/img/categories/2.jpg" alt="martfury" />
+                        <p>Clothings</p>
+                    </div>
+                </div>
+                <div className="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div className="ps-block--category">
+                        <Link href="/shop">
+                            <a className="ps-block__overlay"></a>
+                        </Link>
+                        <img src="/static/img/categories/3.jpg" alt="martfury" />
+                        <p>Computers</p>
+                    </div>
+                </div>
+                <div className="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div className="ps-block--category">
+                        <Link href="/shop">
+                            <a className="ps-block__overlay"></a>
+                        </Link>
+                        <img src="/static/img/categories/4.jpg" alt="martfury" />
+                        <p>Home & Kitchen</p>
+                    </div>
+                </div>
+                <div className="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div className="ps-block--category">
+                        <Link href="/shop">
+                            <a className="ps-block__overlay"></a>
+                        </Link>
+                        <img src="/static/img/categories/5.jpg" alt="martfury" />
+                        <p>Health & Beauty</p>
+                    </div>
+                </div>
+                <div className="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div className="ps-block--category">
+                        <Link href="/shop">
+                            <a className="ps-block__overlay"></a>
+                        </Link>
+                        <img src="/static/img/categories/6.jpg" alt="martfury" />
+                        <p>Health & Beauty</p>
+                    </div>
+                </div>
+                <div className="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div className="ps-block--category">
+                        <Link href="/shop">
+                            <a className="ps-block__overlay"></a>
+                        </Link>
+                        <img src="/static/img/categories/7.jpg" alt="martfury" />
+                        <p>Jewelry & Watch</p>
+                    </div>
+                </div>
+                <div className="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 ">
+                    <div className="ps-block--category">
+                        <Link href="/shop">
+                            <a className="ps-block__overlay"></a>
+                        </Link>
+                        <img src="/static/img/categories/8.jpg" alt="martfury" />
+                        <p>Technology Toys</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomeDefaultTopCategories;
---
+++ components/shared/headers/HeaderDefault.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+import NavigationDefault from '../navigation/NavigationDefault';
+import HeaderActions from './modules/HeaderActions';
+import MenuCategories from './modules/MenuCategories';
+import SearchHeader from './modules/SearchHeader';
+import { stickyHeader } from '../../../utilities/common-helpers';
+
+class HeaderDefault extends Component {
+    constructor({ props }) {
+        super(props);
+    }
+
+    componentDidMount() {
+        if (process.browser) {
+            window.addEventListener('scroll', stickyHeader);
+        }
+    }
+
+    render() {
+        return (
+            <header
+                className="header header--1"
+                data-sticky="true"
+                id="headerSticky">
+                <div className="header__top">
+                    <div className="ps-container">
+                        <div className="header__left">
+                            <div className="menu--product-categories" style={{display:"block"}}>
+                                <div className="menu__toggle" style={{display:"block"}}>
+                                    <i className="icon-menu" style={{fontSize:'30px'}}></i>
+                                </div>
+                                <div className="menu__content">
+                                    <MenuCategories />
+                                </div>
+                            </div>
+                            <Link href="/">
+                                <a className="ps-logo">
+                                    <img
+                                        src="/static/img/logo_light.png"
+                                        alt="martfury"
+                                    />
+                                </a>
+                            </Link>
+                            <div className="menu--product-categories">
+                                    <span> 
+                                        <Link href="/">
+                                        <img
+                                                src="/static/img/logo_light.png"
+                                                alt="martfury"
+                                        />
+                                        </Link>
+                                    </span>
+                                <div className="menu__content">
+                                    <MenuCategories />
+                                </div>
+                            </div>
+                            {/* <div className="menu--product-categories">
+                                <div className="menu__toggle">
+                                    <i className="icon-menu"></i>
+                                    <span> 
+                                        <Link href="/">
+                                        <img
+                                                src="/static/img/logo_light.png"
+                                                alt="martfury"
+                                        />
+                                        </Link>
+                                    </span>
+                                </div>
+                                <div className="menu__content">
+                                    <MenuCategories />
+                                </div>
+                            </div> */}
+                        </div>
+                        <div className="header__center">
+                            <SearchHeader />
+                        </div>
+                        <div className="header__right">
+                            <HeaderActions />
+                        </div>
+                    </div>
+                </div>
+                {/* <NavigationDefault /> */}
+            </header>
+        );
+    }
+}
+
+export default HeaderDefault;
